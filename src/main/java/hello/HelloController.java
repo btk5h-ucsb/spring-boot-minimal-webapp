968efacffe55a27ad91a49b4e00a38a26b5d8214
Bryan Terce <bryan.terce@gmail.com>
2019-10-03 19:21:52 -0700

    Link to github repo from /

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "<h1>Hello World!</h1>\n" +
+            "<p>This web app is powered by \n" +
+            "<a href='https://github.com/btk5h-ucsb/spring-boot-minimal-webapp'>this github repo</a></p>\n";
     }
-    
+
 }
